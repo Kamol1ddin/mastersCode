0b86cae0fdf4883e354a215e244a7942b720ca16
kamol1ddin <kamoliddin073@gmail.com>
2021-08-21 18:04:53 +0500

    project - masters of code

--- Js/app.js
+++ Js/app.js
@@ -1,6 +1,8 @@
 let nav = document.querySelector('nav')
+let descriptionItem = document.querySelectorAll('.description-item')
 let freeCourses = document.querySelector('.header-free-courses')
-let coursesSale = document.querySelector('.courses-sale-block')
+let coursesSale = document.querySelectorAll('.courses-sale-block')
+let priz = document.querySelector('.free-courses-img')
 
 window.addEventListener('scroll', () =>{
     // if(scrollY === 760){
@@ -10,25 +12,38 @@ window.addEventListener('scroll', () =>{
     if(scrollY === 0){
         freeCourses.classList.add('header-courses-free')
     }
-    if(scrollY === 2000){
+    if(scrollY === 500){
+        descriptionItem.style.opacity = 1
+    }
+    if(scrollY === 1800){
         coursesSale[0].style.transform = 'translateX(0)'
-        coursesSale[0].style.transition = 'all .5s'
+        coursesSale[0].style.transition = 'all .8s'
+        coursesSale[0].style.opacity = 1
     }
-    if(scrollY === 2800){
+    if(scrollY === 2300){
         coursesSale[1].style.transform = 'translateX(0)'
-        coursesSale[1].style.transition = 'all .5s'
+        coursesSale[1].style.transition = 'all .8s'
+        coursesSale[1].style.opacity = 1
     }
-    if(scrollY === 3500){
+    if(scrollY === 3300){
         coursesSale[2].style.transform = 'translateX(0)'
-        coursesSale[2].style.transition = 'all .5s'
+        coursesSale[2].style.transition = 'all .8s'
+        coursesSale[2].style.opacity = 1
     }
-    if(scrollY === 4000){
+    if(scrollY === 4100){
         coursesSale[3].style.transform = 'translateX(0)'
-        coursesSale[3].style.transition = 'all .5s'
+        coursesSale[3].style.transition = 'all .8s'
+        coursesSale[3].style.opacity = 1
     }
-    if(scrollY === 4700){
+    if(scrollY === 4600){
         coursesSale[4].style.transform = 'translateX(0)'
-        coursesSale[4].style.transition = 'all .5s'
+        coursesSale[4].style.transition = 'all .8s'
+        coursesSale[4].style.opacity = 1
+    }
+    if(scrollY === 5500){
+        priz.style.transform = "translateY(0)"
+        priz.style.transition = "all .8s"
+        priz.style.opacity = 1
     }
     console.log(scrollY);
 })
